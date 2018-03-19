40691e7bcbd610d9cad914242e3399f9be0491f9
panghupy <15166396925@163.com>
2018-03-19 11:38:42 +0800

    雷霆战机

--- hero_honor/Tools.py
+++ hero_honor/Tools.py
@@ -4,7 +4,7 @@ from plane_sprites import *
 class Music(object):
 	'''音乐类'''	
 
-	MUSIC_LIST = ['./music/bgm','./music/bgm1']
+	
 	
 	def __init__(self, path):
 		self.path = path
@@ -20,9 +20,3 @@ class Music(object):
 	def unpause_music(self):
 		pygame.mixer.music.unpause()
 
-class Button(object):
-    '''按钮类'''
-
-    def __init__(self):
-    	self.count_mouse = 1
-    	self.count_music = 1
